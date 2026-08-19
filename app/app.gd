ddf43617dbce1ee034b853a35725dd9c831ee1a6
Windchili <windchill70@gmail.com>
2026-08-19 23:00:24 +1000

    add toggle to spawn kaguya

--- app/app.gd
+++ app/app.gd
@@ -2,13 +2,15 @@ extends Node3D
 
 const PLAYER = preload("res://features/player/kaguya.tscn")
 @onready var test_grid = $TestGrid
+@export var spawn_player: bool = true
 
 func _ready() -> void:
-	var player = PLAYER.instantiate()
-	player.connect("ready", player_loaded.bind(player))
-	test_grid.add_child(player)
-	
-	player.position.y = 0.5
+	if spawn_player:
+		var player = PLAYER.instantiate()
+		player.connect("ready", player_loaded.bind(player))
+		test_grid.add_child(player)
+		player.position.y = 0.5
 
 func player_loaded(player: Player) -> void:
 	test_grid.get_node("GridInteractionController").camera = player.camera
+	player.camera.current = true
