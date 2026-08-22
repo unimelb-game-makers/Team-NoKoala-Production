723427e30879cdc61d73736b0ebcae08ff5de2d5
SaltPepperVinegar <Alex226336@gmail.com>
2026-08-23 04:20:20 +1000

    feat: adjust grid that it can be both work as a component and as aprent node

--- features/grid/grid.gd
+++ features/grid/grid.gd
@@ -4,19 +4,23 @@ extends GridMap
 var grid_data: GridData = GridData.new()
 
 ## Moves a block to a new cell position if placement is valid.
-func move_block(block: Block, cell: Vector3i) -> bool:
+func move_block(
+	block: Block,
+	cell: Vector3i,
+	visual_root: Node3D = null,
+) -> bool:
 	remove_block(block)
 	block.block_data.root_cell = cell
 	var can_place = grid_data.add_block(block.block_data)
 	if can_place:
-		move_block_visual(block, cell)
+		move_block_visual(visual_root if visual_root != null else block, cell)
 	return can_place
 
 ## Adds a block to the grid if placement is valid.
-func add_block(block: Block) -> bool:
+func add_block(block: Block, visual_root: Node3D = null) -> bool:
 	var can_place = grid_data.add_block(block.block_data)
 	if can_place:
-		add_block_visual(block)
+		add_block_visual(visual_root if visual_root != null else block)
 	return can_place
 
 ## Removes a block from the grid data.
@@ -25,11 +29,11 @@ func remove_block(block: Block):
 
 ## Updates the visual position of a block to match grid coordinates.
 ## This function doesn't update grid data
-func move_block_visual(block: Block, cell: Vector3i):
+func move_block_visual(visual_root: Node3D, cell: Vector3i):
 	var local_pos = map_to_local(cell)
-	block.position = local_pos
+	visual_root.position = local_pos
 
 ## Adds a block as a child node for visual rendering.
 ## This function doesn't update grid data
-func add_block_visual(block: Block):
-	add_child(block)
+func add_block_visual(visual_root: Node3D):
+	add_child(visual_root)
