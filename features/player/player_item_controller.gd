54d34944b5a3ed52199c0c143789e7d528471260
SaltPepperVinegar <Alex226336@gmail.com>
2026-08-27 22:59:34 +1000

    minor fix: make player_item controller no longer depends on grid controller

--- features/player/player_item_controller.gd
+++ features/player/player_item_controller.gd
@@ -3,7 +3,7 @@ class_name PlayerItemController
 
 var held_factory_item: FactoryItem = null
 @export var camera: Camera3D
-@export var grid_controller: GridInteractionController
+@export var grid: Grid
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not event is InputEventMouseButton:
@@ -33,11 +33,11 @@ func _try_pick_up_item_at_mouse() -> void:
 	get_viewport().set_input_as_handled()
 
 func _try_drop_held_item() -> void:
-	if grid_controller == null:
+	if grid == null:
 		return
 
-	var drop_cell := grid_controller.cell_at_mouse_position()
-	var drop_position := grid_controller.position_at_cell(drop_cell)
+	var drop_cell := cell_at_mouse_position()
+	var drop_position := grid.map_to_local(drop_cell)
 	drop_position.y = 0.167
 
 	if global_position.distance_to(drop_position) > Player.PICKUP_DISTANCE:
@@ -67,3 +67,21 @@ func _factory_item_at_mouse() -> FactoryItem:
 		node = node.get_parent()
 
 	return null
+
+
+func cell_at_mouse_position() -> Vector3i:
+	var mouse_pos := get_viewport().get_mouse_position()
+	var ray_origin := camera.project_ray_origin(mouse_pos)
+	var ray_dir := camera.project_ray_normal(mouse_pos)
+	var ray_end := ray_origin + ray_dir * 1000.0
+	
+	var query := PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+	var result := grid.get_world_3d().direct_space_state.intersect_ray(query)
+	
+	if result:
+		var hit_pos: Vector3 = result.position
+		var cell = grid.local_to_map(hit_pos - (result.normal * (grid.cell_size / 2.0)))
+		cell.y = 0
+		return cell
+	else:
+		return Vector3i()
