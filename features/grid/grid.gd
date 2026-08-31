bcc65ee3c634838cbcc542c29e1fb59a2c05b181
SaltPepperVinegar <Alex226336@gmail.com>
2026-09-01 05:16:54 +1000

    implement block dictionary indexed by coordinates

--- features/grid/grid.gd
+++ features/grid/grid.gd
@@ -2,6 +2,7 @@ class_name Grid
 extends GridMap
 
 var grid_data: GridData = GridData.new()
+var _blocks_by_cell: Dictionary[Vector3i, Block] = {}
 
 ## Moves a block to a new cell position if placement is valid.
 func move_block(
@@ -12,6 +13,7 @@ func move_block(
 	block.block_data.root_cell = cell
 	var can_place = grid_data.add_block(block.block_data)
 	if can_place:
+		_index_block(block)
 		move_block_visual(block, cell)
 	return can_place
 
@@ -19,12 +21,27 @@ func move_block(
 func add_block(block: Block) -> bool:
 	var can_place = grid_data.add_block(block.block_data)
 	if can_place:
+		_index_block(block)
 		add_block_visual(block)
 	return can_place
 
 ## Removes a block from the grid data.
-func remove_block(block: Block):
+func remove_block(block: Block) -> void:
+	var blocking_cells := block.block_data.blocking_cells()
 	grid_data.remove_block(block.block_data)
+	for cell in blocking_cells:
+		if _blocks_by_cell.get(cell) == block:
+			_blocks_by_cell.erase(cell)
+
+## Returns the placed block instance at a blocking cell, or null.
+func get_block_at(cell: Vector3i) -> Block:
+	return _blocks_by_cell.get(cell)
+
+func _index_block(block: Block) -> void:
+	for cell in block.block_data.blocking_cells():
+		var cell_data := grid_data.get_cell_data(cell)
+		if cell_data != null and cell_data.block == block.block_data:
+			_blocks_by_cell[cell] = block
 
 func world_to_cell(world_position: Vector3) -> Vector3i:
 	return local_to_map(to_local(world_position))
