3c4212bc0a44e347f185e2b3c8c97a8951aa07f9
s4005506 <s4005506@student.rmit.edu.au>
2026-09-05 14:50:30 +1000

    chore: setting up test scene

--- features/faith_management/faith_progress_bar.gd
+++ features/faith_management/faith_progress_bar.gd
@@ -4,12 +4,11 @@ class_name FaithProgressBar
 @export var low_faith_threshold: float = 20.0
 @export var default_faith_colour: Color = Color("#5f5f9a")
 @export var low_faith_colour: Color = Color.DARK_RED
-
-@onready var progress_bar: TextureProgressBar = $TextureProgressBar
+@export var progress_bar: TextureProgressBar
 
 func _ready() -> void:
 	FaithManager.faith_changed.connect(on_faith_changed)
-	on_faith_changed(FaithManager.current_faith, FaithManager.max_faith)
+	#on_faith_changed(FaithManager.current_faith, FaithManager.max_faith)
 
 func on_faith_changed(new: float, max: float) -> void:
 	progress_bar.max_value = max # allows new max to be set later in game progression
@@ -20,7 +19,7 @@ func on_faith_changed(new: float, max: float) -> void:
 	
 func update_colour(val: float, max: float):
 	if val <= low_faith_threshold:
-		progress_bar.progress.tint = low_faith_colour
+		progress_bar.tint_progress = low_faith_colour
 	else:
-		progress_bar.progress.tint = default_faith_colour
+		progress_bar.tint_progress = default_faith_colour
 		
