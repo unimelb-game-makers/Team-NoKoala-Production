1a2c728813ad52aff92e9a47099bd0b38a9d7b14
s4005506 <s4005506@student.rmit.edu.au>
2026-09-05 13:43:04 +1000

    feat: setting up faith_manager drain logic and setting as global

--- features/faith_management/faith_progress_bar.gd
+++ features/faith_management/faith_progress_bar.gd
@@ -1,17 +1,15 @@
 extends Control
 class_name FaithProgressBar
 
-@export var faith_manager: FaithManager
 @export var low_faith_threshold: float = 20.0
 @export var default_faith_colour: Color = Color("#5f5f9a")
 @export var low_faith_colour: Color = Color.DARK_RED
 
 @onready var progress_bar: TextureProgressBar = $TextureProgressBar
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# connect some signal to faith manager (on faith changed?)
-	pass # Replace with function body.
+	FaithManager.faith_changed.connect(on_faith_changed)
+	on_faith_changed(FaithManager.current_faith, FaithManager.max_faith)
 
 func on_faith_changed(new: float, max: float) -> void:
 	progress_bar.max_value = max # allows new max to be set later in game progression
