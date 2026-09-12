2cf6f7068f85f5c2519565a92a2c7a8db4e25519
Windchili <windchill70@gmail.com>
2026-09-12 18:04:26 +1000

    feat/resource areas

--- features/factory_system/machines/machine_factory.gd
+++ features/factory_system/machines/machine_factory.gd
@@ -3,6 +3,7 @@ class_name MachineFactory
 enum MachineType {
 	DEMO,
 	CONVEYOR,
+	RESOURCE_AREA,
 }
 
 const _SCENES: Dictionary = {
@@ -11,6 +12,9 @@ const _SCENES: Dictionary = {
 	),
 	MachineType.CONVEYOR: preload(
 		"res://features/factory_system/machines/machine_runtime/demo_conveyor_belt.tscn"
+	),
+	MachineType.RESOURCE_AREA: preload(
+		"res://features/resource_area/resource_area_machine.tscn"
 	),
 }
 
---
+++ features/resource_area/resource_area_definition.gd
@@ -0,0 +1,5 @@
+class_name ResourceAreaDefinition
+extends Resource
+
+@export var spawner_recipe: ProductionRecipe
+@export var texture: Texture2D
