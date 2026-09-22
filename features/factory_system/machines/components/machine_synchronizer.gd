c4d6a0b8dedd05feef8c63a503fa82341522bd93
SaltPepperVinegar <Alex226336@gmail.com>
2026-09-22 19:21:40 +1000

    refactor: isolate block synchronizer within the machine synchronizer

--- features/factory_system/machines/components/machine_synchronizer.gd
+++ features/factory_system/machines/components/machine_synchronizer.gd
@@ -1,34 +1,20 @@
 @tool
 class_name MachineSynchronizer
-extends Node
+extends BlockSynchronizer
 
-@export var block: Block
-@export var grid: Grid
 @export var machine: Machine
-@export var auto_snap_position := true
-@export var auto_snap_rotation := true
 
 
 @export_tool_button("Sync Machine", "Callable")
 var sync_machine_button := sync_machine_to_block_data
 
-@export_tool_button("Sync Position", "Callable")
-var sync_position_button := sync_position_to_block_data
-
-@export_tool_button("Sync Rotation", "Callable")
-var sync_rotation_button := sync_rotation_to_block_data
-
 var _watched_definition: MachineDefinition
 var _watched_cells: Array[MachineCellDefinition] = []
-var _has_last_position := false
-var _last_position := Vector3.ZERO
-var _has_last_rotation := false
-var _last_rotation := 0.0
 
 
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if not Engine.is_editor_hint():
 		return 
 
@@ -36,29 +22,7 @@ func _process(_delta: float) -> void:
 	if definition != _watched_definition:
 		_watch_definition(definition)
 
-	if block == null or grid == null:
-		return
-
-	var transform_root := block.get_transform_root()
-	var is_selected := _is_selected(transform_root)
-	if not is_selected:
-		snap_to_grid()
-
-	var current_position := transform_root.global_position
-	var current_rotation := transform_root.global_rotation_degrees.y
-	var position_changed := (
-		not _has_last_position
-		or not current_position.is_equal_approx(_last_position)
-	)
-	var rotation_changed := (
-		not _has_last_rotation
-		or not is_equal_approx(current_rotation, _last_rotation)
-	)
-
-	if position_changed:
-		sync_position_to_block_data()
-	if rotation_changed:
-		sync_rotation_to_block_data()
+	super._process(delta)
 
 
 func _exit_tree() -> void:
@@ -85,44 +49,6 @@ func sync_machine_to_block_data() -> void:
 	block.block_data.footprint = footprint
 	block.block_data.overlap_cells = overlap_cells
 	block.block_data.emit_changed()
-
-
-func sync_position_to_block_data() -> void:
-	if not Engine.is_editor_hint() or block == null or grid == null:
-		return
-
-	var transform_root := block.get_transform_root()
-	_last_position = transform_root.global_position
-	_has_last_position = true
-	if block.block_data != null:
-		block.block_data.root_cell = grid.world_to_cell(_last_position)
-		block.block_data.emit_changed()
-
-
-func sync_rotation_to_block_data() -> void:
-	if not Engine.is_editor_hint() or block == null or grid == null:
-		return
-
-	var transform_root := block.get_transform_root()
-	_last_rotation = transform_root.global_rotation_degrees.y
-	_has_last_rotation = true
-	if block.block_data != null:
-		block.block_data.block_rotation = grid.world_to_grid_rotation(_last_rotation)
-		block.block_data.emit_changed()
-
-
-func snap_to_grid() -> void:
-	var transform_root := block.get_transform_root()
-	if auto_snap_position:
-		var current_cell := grid.world_to_cell(transform_root.global_position)
-		transform_root.global_position = grid.cell_to_world(current_cell)
-	if auto_snap_rotation:
-		var grid_rotation := grid.world_to_grid_rotation(
-			transform_root.global_rotation_degrees.y,
-		)
-		transform_root.global_rotation_degrees.y = (
-			grid.grid_to_world_rotation(grid_rotation)
-		)
 
 
 func _watch_definition(definition: MachineDefinition) -> void:
@@ -165,7 +91,3 @@ func _on_definition_changed() -> void:
 
 func _on_machine_cell_changed() -> void:
 	sync_machine_to_block_data()
-
-
-func _is_selected(node: Node) -> bool:
-	return EditorInterface.get_selection().get_selected_nodes().has(node)
