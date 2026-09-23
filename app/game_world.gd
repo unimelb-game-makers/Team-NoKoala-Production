41f732ad71e208dcf9b56593281f49f33bbea8d3
wirelessEye <okuloj@outlook.com>
2026-09-23 14:05:42 +1000

    fix: fix npc configuration error in game_world

--- app/game_world.gd
+++ app/game_world.gd
@@ -59,7 +59,7 @@ func configure_dependencies() -> void:
 	spring_arm.configure(player)
 	for child in mobs_root.get_children():
 		if child is Npc:
-			child.configure(clock, jobs, reservations, grid, pathfinder)
+			child.configure(clock, jobs, reservations, grid, pathfinder, faith)
 
 func shutdown() -> void:
 	pass
